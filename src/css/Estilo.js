28061e6dd5e36e0593d5adc456698dc0fb1899f9
Luiz G Thomaz <luiz13lg13@gmail.com>
2019-12-02 18:07:11 -0300

    Enviar dados de localização

--- src/css/Estilo.js
+++ src/css/Estilo.js
@@ -1,5 +1,12 @@
 import { StyleSheet, Dimensions, Platform } from 'react-native'
 
 export default StyleSheet.create({
-    
+    Dados:{
+        margin: 30,
+    },
+
+    DadosTexto:{
+        fontSize: 20,
+        marginBottom: 5
+    }
 })
--- src/telas/Principal.js
+++ src/telas/Principal.js
@@ -1,49 +1,103 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import axios from 'axios';
 
-export default class Principal extends Component{
+import Estilos from '../css/Estilo'
 
-    state={
+export default class Principal extends Component {
+
+    state = {
         latitude: 0,
         longitude: 0,
         acuracia: 0,
         altitude: 0,
+        data: 0
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         this.pegarLocalizacao;      // enquanto a localização for atualizada atualiza a renderizacao
     }
 
     // geolocation.watchPosition(success, [error], [options]);
 
-    pegarLocalizacao = () => {       //busca as coordenadas para atualizar a posicao dos pontos
-        navigator.geolocation.getCurrentPosition(
+    pegarLocalizacao() {       //busca as coordenadas para atualizar a posicao dos pontos
+        var posAuxiliar;
+
+        var dia = new Date().getDate();        //Current Date
+        var mes = new Date().getMonth() + 1;  //Current Month
+        var ano = new Date().getFullYear();    //Current Year
+        var hora = new Date().getHours();      //Current Hours
+        var minuto = new Date().getMinutes();      //Current Minutes
+        var seg = new Date().getSeconds();      //Current Seconds
+
+        // var date = dia + '/' + mes + '/' + ano;
+        var date = hora + ':' + minuto + ':' + seg;
+
+        Geolocation.getCurrentPosition(
             (pos) => {
                 posAuxiliar = JSON.stringify(pos);
                 posAuxiliar = JSON.parse(posAuxiliar);
-
                 this.setState({
                     latitude: posAuxiliar.coords.latitude,
                     longitude: posAuxiliar.coords.longitude,
                     acuracia: posAuxiliar.coords.accuracy,
                     altitude: posAuxiliar.coords.altitude,
+                    data: date,
                     error: null
                 });
             },
             (error) => this.setState({ erro: error.message }),
-            {   enableHighAccuracy: false,    //alta precisao
+            {
+                enableHighAccuracy: false,    //alta precisao
                 timeout: 20000,             //tempo para executar antes de retornar erro
-                maximumAge: 1000 },         //tempo permitido de cache do dispositivo
-        )
+                maximumAge: 1000
+            },         //tempo permitido de cache do dispositivo
+        );
     }
 
-    render(){
-        // this.pegarLocalizacao;
-        return(
-            <View>
-                <Text>Latitude: </Text>
-                <Text>{this.state.latitude}</Text>
+    enviarDados = async () => {
+        await axios({
+            method: 'post',
+            url: 'http://192.168.137.1/TCON/Controllers/Map.php',
+            data: {
+                coords:{
+                    lat: this.state.latitude,
+                    lng: this.state.longitude
+                },
+                data: this.state.data
+            }
+        }).then(response => {
+            console.warn(response.data)
+        }).catch(err => {
+            console.warn(err)
+        })
+        
+        // axios({
+        //     method: 'post',
+        //     url: 'http://192.168.137.1/TCON/Controllers/Map.php',
+        //     data:{
+        //         weslley: 'é fodao',
+        //     },
+        // }).then(response => {
+        //     console.warn(response)
+        // }).catch(err => {
+        //     console.warn(err)
+        // })
+    }
 
+    render() {
+        this.pegarLocalizacao();
+        return (
+            <View style={Estilos.Dados}>
+                <Text style={Estilos.DadosTexto}>Latitude: {this.state.latitude}</Text>
+                <Text style={Estilos.DadosTexto}>Longitude: {this.state.longitude}</Text>
+                <Text style={Estilos.DadosTexto}>Acuracia: {this.state.acuracia}</Text>
+                <Text style={Estilos.DadosTexto}>Altitude: {this.state.altitude}</Text>
+                <Text style={Estilos.DadosTexto}>Data: {this.state.data}</Text>
+                <TouchableOpacity onPress={this.enviarDados}>
+                    <Text>Enviar Dados</Text>
+                </TouchableOpacity>
             </View>
         )
     }
